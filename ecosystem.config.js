381377929c0276dadbac3b21763589fa0c9bf02e
Vladimir Saakyan <polossless@gmail.com>
2019-06-05 23:35:52 +0300

    Change ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
 
   deploy : {
     production : {
-      key: "/root/.ssh/id_rsa.pem",
+      key: "~/.ssh/id_rsa",
       user : 'root',
       host : '185.69.152.78',
       ref  : 'origin/master',
