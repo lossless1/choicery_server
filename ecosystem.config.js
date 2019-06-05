e765512153049005a4fe7935ddb76a9815c8d2c3
Vladimir Saakyan <polossless@gmail.com>
2019-06-05 23:27:57 +0300

    change ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  apps : [{
+    name: 'API',
+    script: 'node index.ts',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: true,
+    max_memory_restart: '2G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      key: "~/.ssh/id_rsa.pem",
+      user : 'root',
+      host : '185.69.152.78',
+      ref  : 'origin/master',
+      repo : 'git@github.com:lossless1/choicery_server.git',
+      "ssh_options": "StrictHostKeyChecking=no",
+      path : '/var/www/www-root/data/www/app.choicery.app',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
+//vpz6YQ9V2z45
